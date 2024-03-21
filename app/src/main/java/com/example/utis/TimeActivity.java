8530c7297c6e7d0a038e9fac0810d0b2bc1dc0f2
BounishR <bounish.r2020@vitstudent.ac.in>
2024-03-21 13:04:22 +0530

    UI enhancements, navigation, complete data, text guide

---
+++ app/src/main/java/com/example/utis/TimeActivity.java
@@ -0,0 +1,36 @@
+package com.example.utis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class TimeActivity extends AppCompatActivity {
+    TextView temp;
+    ImageView backImg;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+
+
+        temp=findViewById(R.id.timely);
+        Intent intent=getIntent();
+        String str=intent.getStringExtra("message");
+        temp.setText(str);
+
+
+        backImg = findViewById(R.id.imageBackView);
+        //Back Button
+        backImg.setOnClickListener(v -> {
+            Intent intent1 = new Intent( this,HomeActivity.class); // Use MainActivity.this as the context
+            startActivity(intent1);
+            finish();
+        });
+
+    }
+}

bd4820f4428e16cfb1b77ef038089decf19aef5d
BounishR <bounish.r2020@vitstudent.ac.in>
2024-05-01 16:39:17 +0530

    new progress bar to main page

--- app/src/main/java/com/example/utis/MainActivity.java
+++ app/src/main/java/com/example/utis/MainActivity.java
@@ -4,13 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View; // Import View
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private Handler handler;
+    private int progressStatus = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        handler = new Handler();
+
+        // Start a timer to update progress
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus++;
+                    // Update the progress bar on the UI thread
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+
+                    try {
+                        // Sleep for 20 milliseconds to simulate progress
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        // Hide the progress bar after 2 seconds
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.GONE);
+            }
+        }, 3000);
 
         // Create a Handler to delay the navigation
         new Handler().postDelayed(new Runnable() {
@@ -22,6 +60,6 @@ public class MainActivity extends AppCompatActivity {
                 // Finish the current activity
                 finish();
             }
-        }, 1); // 2000 milliseconds = 2 seconds delay
+        }, 3000); // 2000 milliseconds = 2 seconds delay
     }
 }
